f0df9399fb2ab90fc569f972cc7a7bc8a5fcd670
Diana Papp <pappdiana148@gmail.com>
2024-11-23 00:54:31 +0200

    Add Spline 3D planets with rotation on mouse click and staggered animation for facts list
    - Integrated Spline 3D planets into the planet animation, with Spline's built-in mouse click rotation functionality.
    - Enhanced facts list with staggered animations for a  dynamic appearance.

--- components/planetDetails/PlanetAnimation.jsx
+++ components/planetDetails/PlanetAnimation.jsx
@@ -1,134 +1,178 @@
 'use client'
 
-import { useState } from 'react';
 import classes from './PlanetDetails.module.css'
-import { AnimatePresence, motion } from 'framer-motion';
 import Image from 'next/image';
 import Button from '@/UI/button/Button';
+import Spline from '@splinetool/react-spline';
 
-const planetImageAnimation = {
-    mercury:{
-        dimensionScale: 1.2,
-        orbitSpeed: 1,
-        rotationSpeed: 10
-    },
-    venus:{
-        dimensionScale: 1.8,
-        orbitSpeed: 1.5,
-        rotationSpeed: 35
-    },
-    earth:{
-        dimensionScale: 2,
-        orbitSpeed: 1.8,
-        rotationSpeed: 0.8
-    },
-    mars:{
-        dimensionScale: 1.6,
-        orbitSpeed: 2,
-        rotationSpeed: 0.9
-    },
-    jupiter:{
-        dimensionScale: 3.5,
-        orbitSpeed: 4,
-        rotationSpeed: 0.1
-    },
-    saturn:{
-        dimensionScale: 3.0,
-        orbitSpeed: 4.6,
-        rotationSpeed: 0.2
-    },
-    uranus:{
-        dimensionScale: 2.5,
-        orbitSpeed: 5,
-        rotationSpeed: 0.5
-    },
-    neptune:{
-        dimensionScale: 2.4,
-        orbitSpeed: 5.5,
-        rotationSpeed: 0.4
-    },
+const splineScenes = {
+    mercury: 'https://prod.spline.design/9Y4FnW1vWPVuRqxL/scene.splinecode',
+    venus: 'https://prod.spline.design/nV8IvwgTXDB6sNjK/scene.splinecode',
+    earth: 'https://prod.spline.design/rhHFzLeHOIqaIog6/scene.splinecode',
+    mars: 'https://prod.spline.design/mI4LNtnZn0QYceHS/scene.splinecode',
+    jupiter: 'https://prod.spline.design/6sXJDAB21oILP1Fn/scene.splinecode',
+    saturn: 'https://prod.spline.design/qw5GN-0OLk-nIgbd/scene.splinecode',
+    uranus: 'https://prod.spline.design/vdUF7eWkOG4wMN4d/scene.splinecode',
+    neptune: 'https://prod.spline.design/eXtpzGEVj1JHbc9r/scene.splinecode',
+  };
 
-}
+
 
 export default function PlanetAnimation({planet}){
-    const{ slug, image, dimension, orbit, rotation } = planet;
-    const[animationType, setAnimationType] = useState(null);
-    const[isAnimating, setIsAnimating] = useState(false);
-    const animatedPlanet = planetImageAnimation[slug];
-
-    console.log(animatedPlanet);
+    const{ slug, dimension, orbit, rotation } = planet;
     
-
-    function handleClickAnimation(animationType){
-        setAnimationType(false);
-        setTimeout(() => { // delay to help reset the previous transition when switching from one button to another
-            setAnimationType(animationType);
-            setIsAnimating(true);
-          }, 100)
-        
-    }
-
-    function handleStopAnimation(){
-        setAnimationType(null);
-        setIsAnimating(false);
-    }
-
-
-    console.log(animatedPlanet);
-
-    const animatePlanet ={
-        dimension: { 
-            scale:animatedPlanet.dimensionScale, 
-            x: 0, 
-            rotate: 0, 
-            transition: { repeat: isAnimating ? Infinity : 0, duration: 3, ease: "linear" }, 
-        },
-        orbit: { 
-            scale: 1, 
-            x: "100vw", 
-            rotate: 0,
-            transition: { repeat: isAnimating ? Infinity : 0, duration: animatedPlanet.orbitSpeed, ease: "linear" }, 
-        },
-        rotation: {
-            scale: 1, 
-            x: 0, 
-            rotate: slug === 'venus' ? -360 : slug === 'uranus' ? 90 : 360,
-            transition: { repeat: isAnimating ? Infinity : 0, duration: animatedPlanet.rotationSpeed, ease: "linear" }, 
-        },
-        default: {
-            scale: 1, 
-            x: 0, 
-            rotate: 0,
-            transition: { repeat: 0, duration: 0.5, ease: "easeOut" },
-        }
-    }
 
     return(
         <div className={classes.planetAnimationWrapper}>
-            <AnimatePresence>
-                <motion.div
-                    animate={animatePlanet[animationType] || animatePlanet.default}
-                >
-                    <Image src={image} alt={slug} width={slug === 'saturn' ? 200 : 100} height={100}/>
-                </motion.div>
+            <div className={classes.splineWrapper}>
+                <Spline scene={splineScenes[slug]}  />
 
-            </AnimatePresence>
+            </div>
+    
             <div className={classes.buttonsWrapper}>
                 <div className={classes.animationActions}>
                     <p>Dimension: <span className={slug}>{dimension}</span></p>
-                    <Button onClick={() => handleClickAnimation('dimension')}>Animate</Button>
+                    {/* <Button onClick={() => handleClickAnimation('dimension')}>Animate</Button> */}
                 </div>
                 <div className={classes.animationActions}>
                     <p>Orbit around sun: <span className={slug}>{orbit}</span></p>
-                    <Button onClick={() => handleClickAnimation('orbit')}>Animate</Button>
+                    {/* <Button onClick={() => handleClickAnimation('orbit')}>Animate</Button> */}
                 </div>
                 <div className={classes.animationActions}>
                     <p>Rotation around it&apos;s own axis: <span className={slug}>{rotation}</span></p>
-                    <Button onClick={() => handleClickAnimation('rotation')}>Animate</Button>
+                    {/* <Button onClick={() => handleClickAnimation('rotation')}>Animate</Button> */}
                 </div>
             </div>
-            <Button onClick={handleStopAnimation} className={slug}>STOP ANIMATION</Button>
+            {/* <Button onClick={handleStopAnimation} className={slug}>STOP ANIMATION</Button> */}
 
         </div>
     )
 }
+
+// export default function PlanetAnimation({planet}){
+//     const{ slug, image, dimension, orbit, rotation } = planet;
+//     const[animationType, setAnimationType] = useState(null);
+//     const[isAnimating, setIsAnimating] = useState(false);
+//     const animatedPlanet = planetImageAnimation[slug];
+
+//     console.log(animatedPlanet);
+    
+
+//     function handleClickAnimation(animationType){
+//         setAnimationType(false);
+//         setTimeout(() => { // delay to help reset the previous transition when switching from one button to another
+//             setAnimationType(animationType);
+//             setIsAnimating(true);
+//           }, 100)
+        
+//     }
+
+//     function handleStopAnimation(){
+//         setAnimationType(null);
+//         setIsAnimating(false);
+//     }
+
+
+//     console.log(animatedPlanet);
+
+//     const animatePlanet ={
+//         dimension: { 
+//             scale:animatedPlanet.dimensionScale, 
+//             x: 0, 
+//             rotate: 0, 
+//             transition: { repeat: isAnimating ? Infinity : 0, duration: 3, ease: "linear" }, 
+//         },
+//         orbit: { 
+//             scale: 1, 
+//             x: "100vw", 
+//             rotate: 0,
+//             transition: { repeat: isAnimating ? Infinity : 0, duration: animatedPlanet.orbitSpeed, ease: "linear" }, 
+//         },
+//         rotation: {
+//             scale: 1, 
+//             x: 0, 
+//             rotate: slug === 'venus' ? -360 : slug === 'uranus' ? 90 : 360,
+//             transition: { repeat: isAnimating ? Infinity : 0, duration: animatedPlanet.rotationSpeed, ease: "linear" }, 
+//         },
+//         default: {
+//             scale: 1, 
+//             x: 0, 
+//             rotate: 0,
+//             transition: { repeat: 0, duration: 0.5, ease: "easeOut" },
+//         }
+//     }
+
+//     return(
+//         <div className={classes.planetAnimationWrapper}>
+//             <AnimatePresence>
+//                 <motion.div
+//                     animate={animatePlanet[animationType] || animatePlanet.default}
+//                 >
+//                     <Spline scene={splineScenes[slug]}/>
+//                 </motion.div>
+
+//             </AnimatePresence>
+//             <div className={classes.buttonsWrapper}>
+//                 <div className={classes.animationActions}>
+//                     <p>Dimension: <span className={slug}>{dimension}</span></p>
+//                     <Button onClick={() => handleClickAnimation('dimension')}>Animate</Button>
+//                 </div>
+//                 <div className={classes.animationActions}>
+//                     <p>Orbit around sun: <span className={slug}>{orbit}</span></p>
+//                     <Button onClick={() => handleClickAnimation('orbit')}>Animate</Button>
+//                 </div>
+//                 <div className={classes.animationActions}>
+//                     <p>Rotation around it&apos;s own axis: <span className={slug}>{rotation}</span></p>
+//                     <Button onClick={() => handleClickAnimation('rotation')}>Animate</Button>
+//                 </div>
+//             </div>
+//             <Button onClick={handleStopAnimation} className={slug}>STOP ANIMATION</Button>
+
+//         </div>
+//     )
+// }
+
+
+// const planetImageAnimation = {
+//     mercury:{
+//         dimensionScale: 1.2,
+//         orbitSpeed: 1,
+//         rotationSpeed: 10 // 15-done
+//     },
+//     venus:{
+//         dimensionScale: 1.8,
+//         orbitSpeed: 1.5,
+//         rotationSpeed: 35 //35   done -360
+//     },
+//     earth:{
+//         dimensionScale: 2,
+//         orbitSpeed: 1.8,
+//         rotationSpeed: 0.8 //2.8-done
+//     },
+//     mars:{
+//         dimensionScale: 1.6,
+//         orbitSpeed: 2,
+//         rotationSpeed: 0.9 //2.5-done
+//     },
+//     jupiter:{
+//         dimensionScale: 3.5,
+//         orbitSpeed: 4,
+//         rotationSpeed: 0.1 // 0.5 done
+//     },
+//     saturn:{
+//         dimensionScale: 3.0,
+//         orbitSpeed: 4.6,
+//         rotationSpeed: 0.2 //0.9-done
+//     },
+//     uranus:{
+//         dimensionScale: 2.5,
+//         orbitSpeed: 5,
+//         rotationSpeed: 0.5 // 1.2-done  90
+//     },
+//     neptune:{
+//         dimensionScale: 2.4,
+//         orbitSpeed: 5.5,
+//         rotationSpeed: 0.4 //1.1-done
+//     },
+
+// }
--- components/planetDetails/PlanetDetails.jsx
+++ components/planetDetails/PlanetDetails.jsx
@@ -1,8 +1,10 @@
 'use client'
 
-
 import PlanetAnimation from './PlanetAnimation';
 import classes from './PlanetDetails.module.css';
+import { IoMdArrowDropup } from "react-icons/io";
+import { AnimatePresence, motion } from 'framer-motion';
+import { useState } from 'react';
 
 
 
@@ -10,20 +12,65 @@ import classes from './PlanetDetails.module.css';
 
 export default function PlanetDetails({planet}){
     const{ facts} = planet;
+    const[isRotated, setIsRotated] = useState(false);
+    const[isVisible, setIsVisible] = useState(false);
+
+    function handleClick(){
+        setIsRotated(prevState => !prevState)
+        setIsVisible(prevState => !prevState);
+    }
+
    
+    const staggerContainer = {
+        initial: {opacity: 0},
+        animate: {opacity: 1 , transition : {staggerChildren: 0.5 }},
+        exit:{opacity: 0},
+        child: {
+            initial: { opacity: 0, y: 10},
+            animate: { opacity: 1, y: 0, transition: {duration: 0.5}},
+            exit: {opacity: 0}
+        }
+    }
 
     
     
     return(
         <div className={classes.planetDetailsWrapper}>
            <PlanetAnimation planet={planet}/>
-           <div>
-                <p> Interesting facts about this planet </p>
-                <ul>
-                    {facts.map((fact) => (
-                        <li key={fact}>{fact}</li>
-                    ))}
-                </ul>
+           <div className={classes.facts}>
+                <div className={classes.titleFacts}>
+                    <p> Interesting facts about this planet  </p>
+                    <motion.span
+                        initial={{rotate: 0}}
+                        animate={{rotate: isRotated ? 180 : 0}}
+                        transition={{type: 'spring', stiffness: 200, damping: 30}}
+                        onClick={handleClick}
+                    >
+                        <IoMdArrowDropup />
+                    </motion.span>
+                </div>
+                
+                <AnimatePresence mode='wait'>
+                    {isVisible && (
+                        <motion.ul
+                            variants={staggerContainer}
+                            initial='initial'
+                            animate='animate'
+                            exit='exit'
+                        >
+                            {facts.map((fact) => (
+                                 <motion.li 
+                                    key={fact} 
+                                    className={classes.fact}
+                                    variants={staggerContainer.child}
+                                >
+                                    {fact}
+                                </motion.li>
+                            ))}
+
+                        </motion.ul>
+                    )}
+                </AnimatePresence>
            </div>
         </div>
     )
